c5bfe2bda894c71927edf80632d5e313add7b895
aGamer106 <agamer106@yahoo.com>
2023-11-24 18:40:37 +0000

    Task 2 & 3 Finished + Error Messages thrown

--- app.js
+++ app.js
@@ -6,14 +6,32 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'static')))
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
+
+  try {
+    const stockSymbols = await stocks.getStocks()
+    res.send({ availableStocks: stockSymbols })
+  } catch (error) {
+    console.error('Error getting available stocks: ', erorr.message);
+    res.status(500).send({error: 'Failed to retrieve available stocks'})
+  }
+
 })
 
 app.get('/stocks/:symbol', async (req, res) => {
   const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
+  
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date())
+    res.send(data)
+  } catch (error) {
+    console.error(`Error getting stock data for ${symbol}:`, error.message);
+    res.status(500).send({ error: `Failed to retrieve data for stock ${symbol}` });
+  }
+  
 })
+
+
+
+
 
 app.listen(3000, () => console.log('Server is running!'))
